d1b10fa40aaae21b631e5a6e716f65234d59bffc
DuraAD <dev.amarduraxxx@gmail.com>
2020-03-19 19:43:49 +0545

    News Fragment Added and almost setup completed

--- COVID19/app/src/main/java/com/example/covid_19/ui/news/NewsFragment.java
+++ COVID19/app/src/main/java/com/example/covid_19/ui/news/NewsFragment.java
@@ -1,32 +1,88 @@
 package com.example.covid_19.ui.news;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.covid_19.R;
+import com.example.covid_19.ui.news.api.ApiClient;
+import com.example.covid_19.ui.news.api.ApiInterface;
+import com.example.covid_19.ui.news.models.Article;
+import com.example.covid_19.ui.news.models.News;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class NewsFragment extends Fragment {
 
+    private static final String API_KEY = "a2aad3f4a7f4482a8f592ad9718ee8be";
 
-    public NewsFragment() {
-        // Required empty public constructor
-    }
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    private RecyclerView newsRecyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Article> articles = new ArrayList<>();
+    private Adapter adapter;
+    private String TAG = NewsFragment.class.getSimpleName();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        View view = inflater.inflate(R.layout.fragment_news, container, false);
+
+        newsRecyclerView = view.findViewById(R.id.newsRecyclerView);
+        layoutManager = new LinearLayoutManager(getActivity());
+        newsRecyclerView.setLayoutManager(layoutManager);
+        newsRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        newsRecyclerView.setNestedScrollingEnabled(false);
+
+        Log.e(TAG, "Inside NewsFragment, onCreateView,  Ready to load JSON");
+        loadJson();
+
+        return view;
+    }
+
+    private void loadJson() {
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+
+        String country = Utils.getCountry();
+
+        Call<News> call;
+        call = apiInterface.getNews(country, API_KEY);
+        call.enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+                if (response.isSuccessful() && response.body().getArticles() != null) {
+
+                    if (!articles.isEmpty()) {
+                        articles.clear();
+                    }
+                    articles = response.body().getArticles();
+                    adapter = new Adapter(articles, getContext());
+                    newsRecyclerView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+
+                } else {
+                    Toast.makeText(getContext(), "No Result!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+
+            }
+        });
+
     }
 }
